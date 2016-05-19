87abe9e2c645e284c9ecc63b2d696f0776c06e7d
yunjoopark <yunjoopark12@gmail.com>
2016-05-20 04:34:42 +0900

    update report

---
+++ your-report/FIGS/convex/convert.bat
@@ -0,0 +1,83 @@
+inkscape	-D	-z	--file=ball_s.svg	--export-pdf=ball_s.pdf
+inkscape	-D	-z	--file=ball_s_cut.svg	--export-pdf=ball_s_c.pdf
+inkscape	-D	-z	--file=ball_s_tree.svg	--export-pdf=ball_s_t.pdf
+				
+inkscape	-D	-z	--file=ball_f.svg	--export-pdf=ball_f.pdf
+inkscape	-D	-z	--file=ball_f_cut.svg	--export-pdf=ball_f_c.pdf
+inkscape	-D	-z	--file=ball_f_tree.svg	--export-pdf=ball_f_t.pdf
+				
+inkscape	-D	-z	--file=ball_1.svg	--export-pdf=ball_1.pdf
+inkscape	-D	-z	--file=ball_1_cut.svg	--export-pdf=ball_1_c.pdf
+inkscape	-D	-z	--file=ball_1_tree.svg	--export-pdf=ball_1_t.pdf
+				
+inkscape	-D	-z	--file=ball_2.svg	--export-pdf=ball_2.pdf
+inkscape	-D	-z	--file=ball_2_cut.svg	--export-pdf=ball_2_c.pdf
+inkscape	-D	-z	--file=ball_2_tree.svg	--export-pdf=ball_2_t.pdf
+				
+				
+inkscape	-D	-z	--file=cube-03_s.svg	--export-pdf=cube-03_s.pdf
+inkscape	-D	-z	--file=cube-03_s_cut.svg	--export-pdf=cube-03_s_c.pdf
+inkscape	-D	-z	--file=cube-03_s_tree.svg	--export-pdf=cube-03_s_t.pdf
+				
+inkscape	-D	-z	--file=cube-03_f.svg	--export-pdf=cube-03_f.pdf
+inkscape	-D	-z	--file=cube-03_f_cut.svg	--export-pdf=cube-03_f_c.pdf
+inkscape	-D	-z	--file=cube-03_f_tree.svg	--export-pdf=cube-03_f_t.pdf
+				
+inkscape	-D	-z	--file=cube-03_1.svg	--export-pdf=cube-03_1.pdf
+inkscape	-D	-z	--file=cube-03_1_cut.svg	--export-pdf=cube-03_1_c.pdf
+inkscape	-D	-z	--file=cube-03_1_tree.svg	--export-pdf=cube-03_1_t.pdf
+				
+inkscape	-D	-z	--file=cube-03_2.svg	--export-pdf=cube-03_2.pdf
+inkscape	-D	-z	--file=cube-03_2_cut.svg	--export-pdf=cube-03_2_c.pdf
+inkscape	-D	-z	--file=cube-03_2_tree.svg	--export-pdf=cube-03_2_t.pdf
+				
+				
+inkscape	-D	-z	--file=cylinder_s.svg	--export-pdf=cylinder_s.pdf
+inkscape	-D	-z	--file=cylinder_s_cut.svg	--export-pdf=cylinder_s_c.pdf
+inkscape	-D	-z	--file=cylinder_s_tree.svg	--export-pdf=cylinder_s_t.pdf
+				
+inkscape	-D	-z	--file=cylinder_f.svg	--export-pdf=cylinder_f.pdf
+inkscape	-D	-z	--file=cylinder_f_cut.svg	--export-pdf=cylinder_f_c.pdf
+inkscape	-D	-z	--file=cylinder_f_tree.svg	--export-pdf=cylinder_f_t.pdf
+				
+inkscape	-D	-z	--file=cylinder_1.svg	--export-pdf=cylinder_1.pdf
+inkscape	-D	-z	--file=cylinder_1_cut.svg	--export-pdf=cylinder_1_c.pdf
+inkscape	-D	-z	--file=cylinder_1_tree.svg	--export-pdf=cylinder_1_t.pdf
+				
+inkscape	-D	-z	--file=cylinder_2.svg	--export-pdf=cylinder_2.pdf
+inkscape	-D	-z	--file=cylinder_2_cut.svg	--export-pdf=cylinder_2_c.pdf
+inkscape	-D	-z	--file=cylinder_2_tree.svg	--export-pdf=cylinder_2_t.pdf
+				
+				
+inkscape	-D	-z	--file=triakisicosahedron_s.svg	--export-pdf=icosa_s.pdf
+inkscape	-D	-z	--file=triakisicosahedron_s_cut.svg	--export-pdf=icosa_s_c.pdf
+inkscape	-D	-z	--file=triakisicosahedron_s_tree.svg	--export-pdf=icosa_s_t.pdf
+				
+inkscape	-D	-z	--file=triakisicosahedron_f.svg	--export-pdf=icosa_f.pdf
+inkscape	-D	-z	--file=triakisicosahedron_f_cut.svg	--export-pdf=icosa_f_c.pdf
+inkscape	-D	-z	--file=triakisicosahedron_f_tree.svg	--export-pdf=icosa_f_t.pdf
+				
+inkscape	-D	-z	--file=triakisicosahedron_1.svg	--export-pdf=icosa_1.pdf
+inkscape	-D	-z	--file=triakisicosahedron_1_cut.svg	--export-pdf=icosa_1_c.pdf
+inkscape	-D	-z	--file=triakisicosahedron_1_tree.svg	--export-pdf=icosa_1_t.pdf
+				
+inkscape	-D	-z	--file=triakisicosahedron_2.svg	--export-pdf=icosa_2.pdf
+inkscape	-D	-z	--file=triakisicosahedron_2_cut.svg	--export-pdf=icosa_2_c.pdf
+inkscape	-D	-z	--file=triakisicosahedron_2_tree.svg	--export-pdf=icosa_2_t.pdf
+				
+				
+inkscape	-D	-z	--file=dioctagonal_dipyramid_s.svg	--export-pdf=pyramid_s.pdf
+inkscape	-D	-z	--file=dioctagonal_dipyramid_s_cut.svg	--export-pdf=pyramid_s_c.pdf
+inkscape	-D	-z	--file=dioctagonal_dipyramid_s_tree.svg	--export-pdf=pyramid_s_t.pdf
+				
+inkscape	-D	-z	--file=dioctagonal_dipyramid_f.svg	--export-pdf=pyramid_f.pdf
+inkscape	-D	-z	--file=dioctagonal_dipyramid_f_cut.svg	--export-pdf=pyramid_f_c.pdf
+inkscape	-D	-z	--file=dioctagonal_dipyramid_f_tree.svg	--export-pdf=pyramid_f_t.pdf
+				
+inkscape	-D	-z	--file=dioctagonal_dipyramid_1.svg	--export-pdf=pyramid_1.pdf
+inkscape	-D	-z	--file=dioctagonal_dipyramid_1_cut.svg	--export-pdf=pyramid_1_c.pdf
+inkscape	-D	-z	--file=dioctagonal_dipyramid_1_tree.svg	--export-pdf=pyramid_1_t.pdf
+				
+inkscape	-D	-z	--file=dioctagonal_dipyramid_2.svg	--export-pdf=pyramid_2.pdf
+inkscape	-D	-z	--file=dioctagonal_dipyramid_2_cut.svg	--export-pdf=pyramid_2_c.pdf
+inkscape	-D	-z	--file=dioctagonal_dipyramid_2_tree.svg	--export-pdf=pyramid_2_t.pdf
---
+++ your-report/FIGS/nonconvex/convert.bat
@@ -0,0 +1,83 @@
+inkscape	-D	-z	--file=DwarfAxeMan-1000_s.svg	--export-pdf=n_DwarfAxeMan_s.pdf
+inkscape	-D	-z	--file=DwarfAxeMan-1000_s_cut.svg	--export-pdf=n_DwarfAxeMan_s_c.pdf
+inkscape	-D	-z	--file=DwarfAxeMan-1000_s_tree.svg	--export-pdf=n_DwarfAxeMan_s_t.pdf
+				
+inkscape	-D	-z	--file=DwarfAxeMan-1000_f.svg	--export-pdf=n_DwarfAxeMan_f.pdf
+inkscape	-D	-z	--file=DwarfAxeMan-1000_f_cut.svg	--export-pdf=n_DwarfAxeMan_f_c.pdf
+inkscape	-D	-z	--file=DwarfAxeMan-1000_f_tree.svg	--export-pdf=n_DwarfAxeMan_f_t.pdf
+				
+inkscape	-D	-z	--file=DwarfAxeMan-1000_1.svg	--export-pdf=n_DwarfAxeMan_1.pdf
+inkscape	-D	-z	--file=DwarfAxeMan-1000_1_cut.svg	--export-pdf=n_DwarfAxeMan_1_c.pdf
+inkscape	-D	-z	--file=DwarfAxeMan-1000_1_tree.svg	--export-pdf=n_DwarfAxeMan_1_t.pdf
+				
+inkscape	-D	-z	--file=DwarfAxeMan-1000_2.svg	--export-pdf=n_DwarfAxeMan_2.pdf
+inkscape	-D	-z	--file=DwarfAxeMan-1000_2_cut.svg	--export-pdf=n_DwarfAxeMan_2_c.pdf
+inkscape	-D	-z	--file=DwarfAxeMan-1000_2_tree.svg	--export-pdf=n_DwarfAxeMan_2_t.pdf
+				
+				
+inkscape	-D	-z	--file=knot-480_s.svg	--export-pdf=n_knot_s.pdf
+inkscape	-D	-z	--file=knot-480_s_cut.svg	--export-pdf=n_knot_s_c.pdf
+inkscape	-D	-z	--file=knot-480_s_tree.svg	--export-pdf=n_knot_s_t.pdf
+				
+inkscape	-D	-z	--file=knot-480_f.svg	--export-pdf=n_knot_f.pdf
+inkscape	-D	-z	--file=knot-480_f_cut.svg	--export-pdf=n_knot_f_c.pdf
+inkscape	-D	-z	--file=knot-480_f_tree.svg	--export-pdf=n_knot_f_t.pdf
+				
+inkscape	-D	-z	--file=knot-480_1.svg	--export-pdf=n_knot_1.pdf
+inkscape	-D	-z	--file=knot-480_1_cut.svg	--export-pdf=n_knot_1_c.pdf
+inkscape	-D	-z	--file=knot-480_1_tree.svg	--export-pdf=n_knot_1_t.pdf
+				
+inkscape	-D	-z	--file=knot-480_2.svg	--export-pdf=n_knot_2.pdf
+inkscape	-D	-z	--file=knot-480_2_cut.svg	--export-pdf=n_knot_2_c.pdf
+inkscape	-D	-z	--file=knot-480_2_tree.svg	--export-pdf=n_knot_2_t.pdf
+				
+				
+inkscape	-D	-z	--file=pooh-392_s.svg	--export-pdf=n_pooh_s.pdf
+inkscape	-D	-z	--file=pooh-392_s_cut.svg	--export-pdf=n_pooh_s_c.pdf
+inkscape	-D	-z	--file=pooh-392_s_tree.svg	--export-pdf=n_pooh_s_t.pdf
+				
+inkscape	-D	-z	--file=pooh-392_f.svg	--export-pdf=n_pooh_f.pdf
+inkscape	-D	-z	--file=pooh-392_f_cut.svg	--export-pdf=n_pooh_f_c.pdf
+inkscape	-D	-z	--file=pooh-392_f_tree.svg	--export-pdf=n_pooh_f_t.pdf
+				
+inkscape	-D	-z	--file=pooh-392_1.svg	--export-pdf=n_pooh_1.pdf
+inkscape	-D	-z	--file=pooh-392_1_cut.svg	--export-pdf=n_pooh_1_c.pdf
+inkscape	-D	-z	--file=pooh-392_1_tree.svg	--export-pdf=n_pooh_1_t.pdf
+				
+inkscape	-D	-z	--file=pooh-392_2.svg	--export-pdf=n_pooh_2.pdf
+inkscape	-D	-z	--file=pooh-392_2_cut.svg	--export-pdf=n_pooh_2_c.pdf
+inkscape	-D	-z	--file=pooh-392_2_tree.svg	--export-pdf=n_pooh_2_t.pdf
+				
+				
+inkscape	-D	-z	--file=1up_mushroom-200_s.svg	--export-pdf=n_mushroom_s.pdf
+inkscape	-D	-z	--file=1up_mushroom-200_s_cut.svg	--export-pdf=n_mushroom_s_c.pdf
+inkscape	-D	-z	--file=1up_mushroom-200_s_tree.svg	--export-pdf=n_mushroom_s_t.pdf
+				
+inkscape	-D	-z	--file=1up_mushroom-200_f.svg	--export-pdf=n_mushroom_f.pdf
+inkscape	-D	-z	--file=1up_mushroom-200_f_cut.svg	--export-pdf=n_mushroom_f_c.pdf
+inkscape	-D	-z	--file=1up_mushroom-200_f_tree.svg	--export-pdf=n_mushroom_f_t.pdf
+				
+inkscape	-D	-z	--file=1up_mushroom-200_1.svg	--export-pdf=n_mushroom_1.pdf
+inkscape	-D	-z	--file=1up_mushroom-200_1_cut.svg	--export-pdf=n_mushroom_1_c.pdf
+inkscape	-D	-z	--file=1up_mushroom-200_1_tree.svg	--export-pdf=n_mushroom_1_t.pdf
+				
+inkscape	-D	-z	--file=1up_mushroom-200_2.svg	--export-pdf=n_mushroom_2.pdf
+inkscape	-D	-z	--file=1up_mushroom-200_2_cut.svg	--export-pdf=n_mushroom_2_c.pdf
+inkscape	-D	-z	--file=1up_mushroom-200_2_tree.svg	--export-pdf=n_mushroom_2_t.pdf
+				
+				
+inkscape	-D	-z	--file=star-4split_s.svg	--export-pdf=n_star_s.pdf
+inkscape	-D	-z	--file=star-4split_s_cut.svg	--export-pdf=n_star_s_c.pdf
+inkscape	-D	-z	--file=star-4split_s_tree.svg	--export-pdf=n_star_s_t.pdf
+				
+inkscape	-D	-z	--file=star-4split_f.svg	--export-pdf=n_star_f.pdf
+inkscape	-D	-z	--file=star-4split_f_cut.svg	--export-pdf=n_star_f_c.pdf
+inkscape	-D	-z	--file=star-4split_f_tree.svg	--export-pdf=n_star_f_t.pdf
+				
+inkscape	-D	-z	--file=star-4split_1.svg	--export-pdf=n_star_1.pdf
+inkscape	-D	-z	--file=star-4split_1_cut.svg	--export-pdf=n_star_1_c.pdf
+inkscape	-D	-z	--file=star-4split_1_tree.svg	--export-pdf=n_star_1_t.pdf
+				
+inkscape	-D	-z	--file=star-4split_2.svg	--export-pdf=n_star_2.pdf
+inkscape	-D	-z	--file=star-4split_2_cut.svg	--export-pdf=n_star_2_c.pdf
+inkscape	-D	-z	--file=star-4split_2_tree.svg	--export-pdf=n_star_2_t.pdf
