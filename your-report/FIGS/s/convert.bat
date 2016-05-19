74c7f14dc6affd48b6ecca5bc19c7ff540749eb4
yunjoopark <yunjoopark12@gmail.com>
2016-05-20 05:23:57 +0900

    update report

--- your-report/FIGS/s/convert.bat
+++
@@ -1,3 +0,0 @@
-inkscape	-D	-z	--file=1up_mushroom-200_2.svg	--export-pdf=n_mushroom_2.pdf
-inkscape	-D	-z	--file=1up_mushroom-200_2_cut.svg	--export-pdf=n_mushroom_2_c.pdf
-inkscape	-D	-z	--file=1up_mushroom-200_2_tree.svg	--export-pdf=n_mushroom_2_t.pdf
